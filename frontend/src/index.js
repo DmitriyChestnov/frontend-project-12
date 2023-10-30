4651f8b13cb4974f2f8f36a83b4aaf6a3d28794c
Dmitriy Chestnov <invheor@gmail.com>
2023-10-30 23:27:21 +0300

    rollbar message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import init from './init.jsx';
-
-const app = async () => {
-  const vdom = await init();
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      { vdom }
-    </React.StrictMode>,
-  );
-};
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import app from './init.jsx';
 
 app();
