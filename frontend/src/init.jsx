f475789714e87b18ad5ada0cd3d92a18b9db8948
Dmitriy Chestnov <invheor@gmail.com>
2023-10-31 08:32:22 +0300

    rb fix

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,27 +1,93 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React from 'react';
-import { Provider, ErrorBoundary } from '@rollbar/react'; // Provider imports 'rollbar'
+import { Provider } from 'react-redux';
+import { io } from 'socket.io-client';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import i18next from 'i18next';
+import resources from './locales/index.js';
+import App from './components/App.jsx';
+import { actions as messagesActions } from './slices/messagesSlice.js';
+import { actions as channelsActions } from './slices/channelsSlice.js';
+import { ChatApiContext } from './contexts/index.jsx';
+import store from './slices/index.js';
 
-const rollbarConfig = {
-  accessToken: 'ae4d160e8f72463896e4500d04ee01cf',
-  environment: 'testenv',
-};
+const defaultLanguage = 'ru';
+const socketTimeoutMs = 5000;
 
-function TestError() {
-  const a = null;
-  return a.hello();
-}
+export default async () => {
+  const i18nextInstance = i18next.createInstance();
+  await i18nextInstance
+    .use(initReactI18next)
+    .init({
+      lng: defaultLanguage,
+      debug: false,
+      resources,
+    });
+  const socket = io();
+  console.debug(`Subscribe for socket events (socket.id=${socket.id})`);
+  socket
+    .on('connect', () => {
+      console.debug(`socket "connect" id=${socket.id}`);
+    })
+    .on('connect', () => {
+      console.debug('socket "connect_error"');
+    })
+    .on('disconnect', (reason) => {
+      console.debug(`socket "disconnect" (${reason})`);
+    })
 
-// Provider instantiates Rollbar client instance handling any
-// uncaught errors or unhandled promises in the browser
-// ErrorBoundary catches all React errors in the tree below and logs them to Rollbar
-// eslint-disable-next-line react/function-component-definition
-export default function App() {
+    .on('newMessage', (payload) => {
+      console.debug('newMessage "event"', payload);
+      store.dispatch(messagesActions.addMessage(payload));
+    })
+    .on('newChannel', (payload) => {
+      console.debug('newChannel "event"', payload);
+      store.dispatch(channelsActions.addChannel(payload));
+      toast.info(i18nextInstance.t('channels.created'));
+    })
+    .on('removeChannel', (payload) => {
+      console.debug('removeChannel "event"', payload);
+      store.dispatch(channelsActions.removeChannel(payload.id));
+      toast.info(i18nextInstance.t('channels.remove'));
+    })
+    .on('renameChannel', (payload) => {
+      console.debug('renameChannel "event"', payload);
+      const { id, name } = payload;
+      store.dispatch(channelsActions.updateChannel({ id, changes: { name } }));
+      toast.info(i18nextInstance.t('channels.renamed'));
+    });
+  const getSocketEmitPromise = (...args) => new Promise((resolve, reject) => {
+    socket.timeout(socketTimeoutMs).emit(...args, (err, response) => {
+      if (err) reject(err);
+      resolve(response);
+    });
+  });
+  const chatApi = {
+    newMessage: (...args) => getSocketEmitPromise('newMessage', ...args),
+    newChannel: (...args) => getSocketEmitPromise('newChannel', ...args),
+    removeChannel: (...args) => getSocketEmitPromise('removeChannel', ...args),
+    renameChannel: (...args) => getSocketEmitPromise('renameChannel', ...args),
+  };
+  const rollbarConfig = {
+    accessToken: 'b8d56ecd19764e53895e319979a09018',
+    captureUncaught: true,
+    environment: 'production',
+  };
   return (
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        <TestError />
-      </ErrorBoundary>
-    </Provider>
+    <I18nextProvider i18n={i18nextInstance}>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <ChatApiContext.Provider value={chatApi}>
+              <App />
+              <ToastContainer pauseOnFocusLoss={false} position="top-center" />
+            </ChatApiContext.Provider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
+    </I18nextProvider>
   );
-}
+};
